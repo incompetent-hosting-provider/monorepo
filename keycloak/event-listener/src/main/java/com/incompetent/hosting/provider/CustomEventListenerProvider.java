a24e5ba183d6873d8acb85f174a4aecaf82fe9f2
coffemakingtoaster <mh340@hdm-stuttgart.de>
2024-02-15 00:26:51 +0100

    feat(keycloak events): add basic webhook call

--- keycloak/event-listener/src/main/java/com/incompetent/hosting/provider/CustomEventListenerProvider.java
+++ keycloak/event-listener/src/main/java/com/incompetent/hosting/provider/CustomEventListenerProvider.java
@@ -1,5 +1,10 @@
 package com.incompetent.hosting.provider;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.logging.Logger;
 
 import org.keycloak.events.Event;
@@ -7,10 +12,15 @@ import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 import org.keycloak.models.KeycloakSession;
+import org.json.JSONObject;
 
 public class CustomEventListenerProvider implements EventListenerProvider {
 
     private static final Logger log = Logger.getLogger(CustomEventListenerProvider.class.getName());
+
+    private final String BackendHost = System.getenv("ICHP_BACKEND_HOST");
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
 
     public CustomEventListenerProvider(KeycloakSession session) {
     }
@@ -18,21 +28,27 @@ public class CustomEventListenerProvider implements EventListenerProvider {
     @Override
     public void onEvent(Event event) {
 
-        if (EventType.REGISTER.equals(event.getType())) {
-            log.info("--------\nReceived register event\n-------------");
-            return;
+        if (EventType.REGISTER.equals(event.getType()) || EventType.DELETE_ACCOUNT.equals(event.getType())) {
+            log.info("--------\nReceived event\n-------------");
+            callBackendWebhook(buildJSONBody(event.getType(), event.getUserId()));
         }
+    }
 
-        if (EventType.DELETE_ACCOUNT.equals(event.getType())){
-            log.info("------\nReceived account delete event\n-----");
-            return;
+    private String buildJSONBody(EventType eventType, String userId){
+        return new JSONObject().put("keycloakEvent", eventType.toString()).put("userId", userId).toString();
+    }
+
+    private void callBackendWebhook(String jsonPayload)  {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(BackendHost + "/spi-webhooks")).POST(HttpRequest.BodyPublishers.ofString( jsonPayload)).build();
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception exception){
+            log.warning("Cannot call backend webhook due to an error" + exception.toString());
         }
-
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-
     }
 
     @Override
