b41fc10c64cae4e0022e8bb25ef5655675525974
Dennis Schmidt <dg085@hdm-stuttgart.de>
2024-02-18 18:17:20 +0100

    :sparkles: Implemented balance cmd

--- cli/cmd/balance/balance.go
+++ cli/cmd/balance/balance.go
@@ -23,6 +23,44 @@ var BalanceCmd = &cobra.Command{
 	Short: "Add short description", // TODO: Add short description
 	Long:  "Add long description",  // TODO: Add long description
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("'ihp balance' called")
+		tokens := authentication.GetCurrentAuthentication()
+		if tokens == nil {
+		utils.DisplayNotLoggedInMessage()
+		return
+		}
+
+		amount, err := backend.GetBalance(*tokens)
+		if err != nil {
+			if errors.Is(err, backend.ErrNotAuthenticated) {
+				// Refresh tokens and try again
+				newTokens, err := authentication.RefreshTokens()
+				if err != nil {
+					displayUnableToGetBalanceMessage(err)
+					os.Exit(1)
+				}
+
+				if newTokens == nil {
+					utils.DisplaySessionExpiredMessage()
+					return
+				}
+
+				amount, err = backend.GetBalance(*newTokens)
+				if err != nil {
+					displayUnableToGetBalanceMessage(err)
+					os.Exit(1)
+				}
+			} else {
+				displayUnableToGetBalanceMessage(err)
+				os.Exit(1)
+			}
+		}
+
+		fmt.Println("Your current balance is:", amount, "credits")
+		fmt.Println("To purchase more credits, run 'ihp balance purchase'")
 	},
+}
+
+func displayUnableToGetBalanceMessage(err error) {
+	fmt.Println("Unable to get your balance:", err)
+	fmt.Println("Please try again later.")
 }
--- cli/internal/backend/backend.go
+++ cli/internal/backend/backend.go
@@ -22,12 +22,10 @@ type UserInfo struct {
 }
 
 func GetUserInfo(tokens authentication.SessionTokens) (*UserInfo, error) {
-	req, err := http.NewRequest("GET", backendBaseURL+"/user", nil)
+	req, err := getAuthenticatedRequest("GET", "/user", tokens.AccessToken)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create user info request: %w", err)
+		return nil, fmt.Errorf("failed to build request for user info: %w", err)
 	}
-
-	req.Header.Add("Authorization", "Bearer "+string(tokens.AccessToken))
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -56,4 +54,53 @@ func GetUserInfo(tokens authentication.SessionTokens) (*UserInfo, error) {
 	}
 
 	return userInfo, nil
+}
+
+type GetBalanceResponse struct {
+	Balance int `json:"balance,omitempty"`
+}
+
+func GetBalance(tokens authentication.SessionTokens) (int, error) {
+	req, err := getAuthenticatedRequest("GET", "/balance", tokens.AccessToken)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create get balance request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get user balance: %w", err)
+	}
+
+	switch resp.StatusCode {
+	case 200:
+		break
+	case 401:
+		return 0, fmt.Errorf("failed to get user balance: %w", ErrNotAuthenticated)
+	default:
+		return 0, fmt.Errorf("failed to get user balance: %s", resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return 0, fmt.Errorf("failed to read response body of user balance request: %w", err)
+	}
+
+	var balanceResponse *GetBalanceResponse
+	err = json.Unmarshal(body, &balanceResponse)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse response body of user balance request: %w", err)
+	}
+
+	return balanceResponse.Balance, nil
+}
+
+func getAuthenticatedRequest(method string, path string, accessToken authentication.AccessToken) (*http.Request, error) {
+	req, err := http.NewRequest("GET", backendBaseURL+"/user", nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create authenticated request: %w", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer " + string(accessToken))
+	return req, nil
 }
