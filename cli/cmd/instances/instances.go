d87726bb883ee7d297bc593508c7413a13c6eda9
Dennis Schmidt <dg085@hdm-stuttgart.de>
2024-02-28 16:59:50 +0100

    :sparkles: Implemented instances

--- cli/cmd/instances/instances.go
+++ cli/cmd/instances/instances.go
@@ -1,6 +1,10 @@
 package instances
 
 import (
+	"cli/internal/authentication"
+	"cli/internal/backend"
+	"cli/internal/messages"
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -11,9 +15,27 @@ import (
 // Lists all instances belonging to a user
 var InstancesCmd = &cobra.Command{
 	Use:   "instances",
-	Short: "Add short description", // TODO: Add short description
-	Long:  "Add long description",  // TODO: Add long description
+	Short: "Lists all instances belonging to a user.",
+	Long:  "Lists all instances belonging to a user. \n Format is: [STATUS] ID - NAME, IMAGE",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("'ihp instances' called")
+		tokens := authentication.GetCurrentAuthentication()
+		if tokens == nil {
+			messages.DisplayNotLoggedInMessage()
+			return
+		}
+
+		instances, err := backend.DefaultBackendClient.GetUserInstances(tokens.AccessToken, true)
+		if errors.Is(err, backend.ErrNotAuthenticated) {
+			messages.DisplaySessionExpiredMessage()
+			return
+		} else if err != nil {
+			fmt.Println("Unable to get your instances:", err)
+			fmt.Println("Please try again later.")
+			return
+		}
+
+		for _, instance := range instances {
+			fmt.Println(instance.String())
+		}
 	},
 }
--- cli/internal/models/instance.go
+++ cli/internal/models/instance.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 
 type InstanceStatus string
@@ -15,12 +19,20 @@ type ContainerImage struct {
 	Version string `json:"version,omitempty"`
 }
 
+func (c ContainerImage) String() string {
+	return fmt.Sprintf("%s:%s", c.Name, c.Version)
+}
+
 type Instance struct {
 	InstanceType string `json:"type,omitempty"`
 	ID string `json:"instance_id,omitempty"`
 	Name string `json:"name,omitempty"`
 	Image ContainerImage `json:"container_image,omitempty"`
 	Status InstanceStatus `json:"status,omitempty"`
+}
+
+func (i Instance) String() string {
+	return fmt.Sprintf("[%s] %s - %s, %s", i.Status, i.ID, i.Name, i.Image.String())
 }
 
 type InstanceDetail struct {
@@ -31,8 +43,32 @@ type InstanceDetail struct {
 	Description string `json:"description,omitempty"`
 }
 
+func (i InstanceDetail) String() string {
+	builder := strings.Builder{}
+	// Generic header as in Instance.String()
+	builder.WriteString(i.Instance.String())
+	builder.WriteString(fmt.Sprintf("\nDescription: %s", i.Description))
+	builder.WriteString(fmt.Sprintf("\nCreated at: %s", i.CreatedAt.Format(time.RFC3339)))
+	if i.Status == Running {
+		builder.WriteString(fmt.Sprintf("\nStarted at: %s", i.StartedAt.Format(time.RFC3339)))
+	}
+
+	if len(i.OpenPorts) > 0 {
+		builder.WriteString("\nOpen Ports: ")
+		for _, port := range i.OpenPorts {
+			builder.WriteString(fmt.Sprintf("%d ", port))
+		}
+	}
+
+	return builder.String()
+}
+
 type InstancePreset struct {
 	ID string `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`	
 	Description string `json:"description,omitempty"`
+}
+
+func (i InstancePreset) String() string {
+	return fmt.Sprintf("%s: %s - %s", i.ID, i.Name, i.Description)
 }
