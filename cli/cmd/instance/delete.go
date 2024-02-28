ea97a7c370f05e04cd924032726471db0b09b44e
Dennis Schmidt <dg085@hdm-stuttgart.de>
2024-02-28 16:59:30 +0100

    :sparkles: Implemented delete

--- cli/cmd/instance/delete.go
+++ cli/cmd/instance/delete.go
@@ -1,20 +1,42 @@
 package instance
 
 import (
+	"cli/internal/authentication"
+	"cli/internal/backend"
+	"cli/internal/messages"
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-
 // Instances Delete Command
 //
 // Allows the user to delete an instance
 var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "Add short description", // TODO: Add short description
-	Long:  "Add long description",  // TODO: Add long description
+	Use:   "delete [instance_id]",
+	Short: "Deletes an instance.", 
+	Long:  "Deletes an instance.",
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("'ihp instance delete' called")
+		id := args[0]
+		
+		tokens := authentication.GetCurrentAuthentication()
+		if tokens == nil {
+			messages.DisplayNotLoggedInMessage()
+			return
+		}
+
+		err := backend.DefaultBackendClient.DeleteInstance(tokens.AccessToken, id, true)
+		if errors.Is(err, backend.ErrNotAuthenticated) {
+			messages.DisplaySessionExpiredMessage()
+			return
+		} else if err != nil {
+			fmt.Println("Unable to delete your instance:", err)
+			fmt.Println("Please try again later.")
+			return
+		}
+
+		fmt.Printf("Instance <<%s>> successfully deleted.", id)
 	},
 }
